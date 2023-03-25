4c53bd9179ae06c04c05a8dd7e091922302635c6
Dino <dino15031992@gmail.com>
2023-03-25 14:43:37 +0100

    Initial commit

---
+++ src/main/java/it/socialNetwork/socialNetworkApp/SocialNetworkAppApplication.java
@@ -0,0 +1,21 @@
+package it.socialNetwork.socialNetworkApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type Social network app application.
+ */
+@SpringBootApplication
+public class SocialNetworkAppApplication {
+
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/socialNetwork/socialNetworkApp/SocialNetworkAppApplicationTests.java
@@ -0,0 +1,19 @@
+package it.socialNetwork.socialNetworkApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * The type Social network app application tests.
+ */
+@SpringBootTest
+class SocialNetworkAppApplicationTests {
+
+    /**
+     * Context loads.
+     */
+    @Test
+	void contextLoads() {
+	}
+
+}
